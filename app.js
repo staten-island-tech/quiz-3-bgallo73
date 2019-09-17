e57cbdd0d1e9b8e630cd1111750ef81d530389e7
blaeugh <50417843+blaeugh@users.noreply.github.com>
2019-09-17 10:59:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,22 @@ function tipCalc(bill) {
     else {
         percent = .2
     }
-    return percentage * bill;
+    return percent * bill;
+}
+
+function total(bill) {
+    return tipCalc(bill) + bill;
 }
 
 const bills = [140, 45, 270];
 const tips = [tipCalc(bills[0]), tipCalc(bills[1]), tipCalc(bills[2])]
-const total = [tip] 
+const fullTotal = [total(140), total(45), total(270)]
+console.log(fullTotal);
+
+function isBigEnough(value) {
+    return value >= 100;
+}
+
+const copy = [total(140), total(45), total(270)].filter(isBigEnough);
+
+console.log(copy)
